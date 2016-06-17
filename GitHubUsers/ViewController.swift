e8681a91ee341358da51995b48798523eee2bd66
Serge Sukhanov <ssuhanov@icloud.com>
2016-06-17 07:52:47 +0300

    Initial Commit

---
+++ GitHubUsers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubUsers
+//
+//  Created by Serge Sukhanov on 17.06.16.
+//  Copyright © 2016 Serge Sukhanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
